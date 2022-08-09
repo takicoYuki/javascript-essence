c2b278e3e74e403ed5896b4eebbda5f6d4e57f31
takita <takita.cons@gmail.com>
2022-08-09 16:30:23 +0900

    関数とオブジェクト2完了

--- 260_修了確認_PubSubパターン/010_回答/main.js
+++ 260_修了確認_PubSubパターン/010_回答/main.js
@@ -4,19 +4,19 @@
  */
 
 //登録する巻子う
-// function customFn1() {
-// 	console.log('customFn1');
-// }
-// function customFn2() {
-// 	console.log('customFn2');
-// }
-// function customFn3() {
-// 	console.log('customFn3');
-// }
+function customFn1() {
+	console.log('customFn1');
+}
+function customFn2() {
+	console.log('customFn2');
+}
+function customFn3() {
+	console.log('customFn3');
+}
 
-//トリガーの場所
-// events.on('beforeInit', customFn1);
-// events.on('beforeInit', customFn2);
-// events.on('afterInit', customFn3);
+// // トリガーの場所
+events.on('beforeInit', customFn1);
+events.on('beforeInit', customFn2);
+events.on('afterInit', customFn3);
 
-// new MyLibrary();
+new MyLibrary();
--- 260_修了確認_PubSubパターン/010_回答/my-library.js
+++ 260_修了確認_PubSubパターン/010_回答/my-library.js
@@ -40,55 +40,78 @@
  *
  */
 
-class Events{
-	constructor(){
-		this.map = new Map();
-	}
-	on(trigger,method){
-		//key,[関数]
-		//もともとkeyがあったら追加する
-		if(this.map.get(trigger)){
+//classで作る
+//setで作ったほうがいい
+//ライブラリだからclass よりツールだからオブジェクトで作ったほうがいい
+// class events{
+// 	static map = new Map();
 
-		}
-		this.map.set(trigger,[method]);
+// 	static on(trigger,method){
+// 		//{key,[関数]}
+// 		if(this.map.get(trigger)){
+// 			let array = this.map.get(trigger);
+// 			array.push(method);
+// 			this.map.set(trigger,array);
+// 		}else{
+// 			this.map.set(trigger, [method]);
+// 		}
+// 	}
+
+// 	static off(trigger,method){
+// 		//triggerでよびだして配列を回して一致するものを探す
+// 		let array = this.map.get(trigger);
+// 		//ここが良くなさそう
+// 		let result = array.filter((array)=>{
+// 			return array !== method;
+// 		});
+// 		this.map.set(trigger, result);
+// 	}
+
+// 	static emit(trigger){
+// 		//trigger別で実行する
+// 		for(let [k,v] of this.map){
+// 			let array = v;
+// 			if(k == trigger){
+// 				for (let key in array) {
+// 					array[key]();
+// 				}
+// 			}
+// 		}
+// 	}
+// }
+
+//オブジェクトで作成する
+const events = function(){
+	//外のスコープからいじれないように即時関数で作成する
+	const map = new Map();
+
+	//setを回すforEach用コールバック関数
+	function run (value1){
+		if(value1){value1();}
 	}
-	emit(trigger){
-		//trigger別で実行する
-		for(let key in this.map){
-			if(key == trigger){
-				//メソッドを実行する
-				this.map[key]();
+	//関数
+	return {
+		on(trigger,method){
+			//関数を登録する
+			//短絡的分岐
+			//mapにデータがあればmapから,なければnew Set
+			let set = map.get(trigger) || new Set();
+			//重複があったら追加されないからこれだけでOK
+			set.add(method);
+			map.set(trigger,set);
+		},
+		off(trigger,method){
+			let set = map.get(trigger);
+			if(set.has(method)){
+				set.delete(method);
 			}
+		},
+		emit(trigger){
+			let set = map.get(trigger);
+			set.forEach(run);
 		}
 	}
-}
-
-const events = new Events();
-//適当なメソッド
-function customFn1() {
-	console.log('customFn1');
-}
-function customFn2() {
-	console.log('customFn2');
-}
-function customFn3() {
-	console.log('customFn3');
-}
-
-//登録してみる
-//同じkeyだと上書きされる
-events.on('beforeInit', customFn1);
-events.on('beforeInit', customFn2);
-events.on('beforeInit', customFn3);
-
-console.log(events.map.get('beforeInit'));
-for(let i in events.map){
-	console.log(events.map.get(i));
-}
-
-//実行してみる
-events.emit('beforeInit');
-
+}();
 
 class MyLibrary {
 	constructor() {
@@ -98,22 +121,50 @@ class MyLibrary {
 
 		events.emit('afterInit');
 	}
-
-	//emit(トリガー)
-	//登録されたトリガーから探して実行する
-
 	method() {
 		// do something
 	}
 }
 
+//-----------テスト--------------------
+//インスタンス作成
+// const events = new Events();
+//適当なメソッド
+// function customFn1() {
+// 	console.log('customFn1');
+// }
+// function customFn2() {
+// 	console.log('customFn2');
+// }
+// function customFn3() {
+// 	console.log('customFn3');
+// }
+
+//登録してみる
+//同じkeyだと上書きされる
+// events.on('beforeInit', customFn1);
+// events.on('beforeInit', customFn2);
+// events.on('beforeInit', customFn3);
+// events.on('afterInit', customFn1);
+// events.on('afterInit', customFn2);
+// events.on('afterInit', customFn3);
+
+//実行してみる
+// events.emit('beforeInit');
+
+//登録を解除してみる
+// events.off('beforeInit', customFn1);
+
+//実行してみる
+// events.emit('beforeInit');
+
 //for in of 忘れた
-const items = {
-	prop1: 'value1',
-	prop2: 'value2',
-	prop3: 'value3',
-	prop4: 'value4',
-}
+// const items = {
+// 	prop1: 'value1',
+// 	prop2: 'value2',
+// 	prop3: 'value3',
+// 	prop4: 'value4',
+// }
 //keyが取得できる
 // for(let item in items){
 // 	console.log(item);
@@ -122,3 +173,21 @@ const items = {
 // for (let item of items) {
 // 	console.log(item);
 // }
+
+//配列
+// const array = [1,2,3,4,5,1,1];
+
+// let result = array.filter((array)=>{
+// 	return array !== 1;
+// });
+// console.log(result);
+
+//set
+// const set = new Set();
+// set.add(() => {
+// 	console.log('test');
+// })
+// function run(value1) {
+// 	if (value1) { value1();}
+// }
+// set.forEach(run);
